1aa28a6b37edeacb390162f813b6f6e91c67428d
reflective-refactoring-7309 <bristuttii@gmail.com>
2020-06-09 23:56:29 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,18 @@
-def my_each # put argument(s) here
+#  def my_each # put argument(s) here
   # code here
+#  end
+
+def my_each
+    if block_given?
+      i = self.count-1
+      n = 0
+      while n <= i
+        yield(self[n])
+        n += 1
+      end
+    else return self
+  end
 end
 
+array = [1,2,3,4,5,6]
+array.my_each { |element| puts element + 1 }
